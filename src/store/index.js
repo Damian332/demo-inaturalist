757690c6087337748227be671d22f1f4c85302a4
webferrol <xurxo@webferrol.com>
2022-04-22 12:37:21 +0200

    Añadido búsqueda y listado de especies de galicia de plantas de la api de  inaturalist

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,29 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    observations: [],
+    total_results: 0,
   },
   mutations: {
+    getObservations(state, payload) {
+      state.observations = payload.results;
+      state.total_results = payload.total_results;
+    }
   },
   actions: {
+    async getObservations({ commit }, { page, query }) {
+      console.log(query, ' tata');
+      try {
+        const response = await fetch(
+          `https://api.inaturalist.org/v1/observations?${query !== '' ? 'q=' + query + '&' : ''}hrank=genus&lrank=species&place_id=6779&reviewed=true&iconic_taxa=Plantae&page=${page}&per_page=200&order=desc&order_by=created_at`
+        );
+        const data = await response.json();
+        console.log(data);
+        commit('getObservations', data);
+      } catch (error) {
+        console.log(error);
+      }
+    },
   },
   modules: {
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,63 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    {{ total_results }}
+    <input v-model.trim="query" type="search" name="" id="">
+    <button @click="dispatchObservations">Pulsar</button>
+
+    <select @change="dispatchObservations" v-model.number="cmbPage" name="" id="">
+      <option v-for="i in count" :value="i">{{ i }}</option>
+
+    </select>
+    <table>
+      <tr>
+        <th>Taxon</th>
+        <th>Imagen</th>
+        <th>Geolocalización</th>
+      </tr>
+      <tr v-for="item in observations" :key="item.id">
+        <td>{{ (item.taxon && item.taxon.name) ? item.taxon.name : '' }}</td>
+        <td><img height="100" :src="`${item.photos[0] && item.photos[0].url ? item.photos[0].url : '#'}`" :alt="item.uri">
+        </td>
+        <td>
+          {{ item.location }}
+        </td>
+      </tr>
+    </table>
+
+
   </div>
 </template>
 
-<script>
+<script setup>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+import { computed, ref } from 'vue';
+import { useStore } from 'vuex';
+
+const store = useStore();
+const cmbPage = ref(1);
+const query = ref('');
+
+
+store.dispatch('getObservations', { page: cmbPage.value, query: query.value });
+
+
+const observations = computed(() => store.state.observations);
+const total_results = computed(() => store.state.total_results);
+const count = computed(() => {
+  let total = store.state.total_results / 200;
+  let paginas = (total - Math.floor(total) === 0) ? total : Math.floor(total) + 1;
+  return paginas > 50 ? 50 : paginas;
+});
+
+
+/**
+ * Métodos
+ */
+
+const dispatchObservations = () => {
+  store.dispatch('getObservations', { page: cmbPage.value, query: query.value });
 }
+
+
 </script>
